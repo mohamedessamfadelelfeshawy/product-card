050fe82d31bb9e91de011622f79af0ed5c3c2407
mohamedessamfadelelfeshawy <ana459925@gmail.com>
2025-05-07 01:02:27 +0300

    custom modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,32 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import SideBar from "./component/SideBar";
 import { productList } from "./data";
+import Modal from "./component/ui/Modal";
 function App() {
+  /* staet */
+  const [isOpen, setIsOpen] = useState(false);
+  /* handel */
+  function close() {
+    setIsOpen(false);
+  }
+  function open() {
+    setIsOpen(true);
+  }
+
+  /* Rener */
   return (
     <Fragment>
-      <div className="container">
+      <main className="container">
+        {/*         //one */}
         <div className="flex items-center justify-between my-4  ">
           <h1 className="font-bold text-2xl">
             Latest <span className="text-blue-700">Products</span>
           </h1>
-          <button className="p-2 bg-blue-700 text-white rounded-md">
+          <button className="p-2 bg-blue-700 text-white rounded-md" onClick={open}>
             Build now
           </button>
         </div>
-
+        {/*  //two */}
         <div
           className=" grid 
       grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 rounded-2xl
@@ -23,7 +36,18 @@ function App() {
             <SideBar key={obj.id} product={obj} />
           ))}
         </div>
-      </div>
+        {/*         //there */}
+        <Modal title={"Add Product"} isOpen={isOpen} close={close}>
+          <div className="flex items-center space-x-2 my-2 ">
+            <button className="bg-blue-600 p-2 rounded-md text-white w-full">
+              Submit
+            </button>
+            <button className="bg-gray-600 p-2 rounded-md text-white w-full">
+              Cancle
+            </button>
+          </div>
+        </Modal>
+      </main>
     </Fragment>
   );
 }
